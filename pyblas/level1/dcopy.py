f554a73a0dd0526591fa65d75bd1574a86d66790
Tim Leslie <tim.leslie@gmail.com>
2020-10-10 20:13:09 +1100

    Update dcopy

--- pyblas/level1/dcopy.py
+++ pyblas/level1/dcopy.py
@@ -1,107 +1,48 @@
-# > \brief \b DCOPY
-#
-#  =========== DOCUMENTATION ===========
-#
-# Online html documentation available at
-#            http://www.netlib.org/lapack/explore-html/
-#
-#  Definition:
-#  ===========
-#
-#       def DCOPY(N,DX,INCX,DY,INCY)
-#
-#       .. Scalar Arguments ..
-#       INTEGER INCX,INCY,N
-#       ..
-#       .. Array Arguments ..
-#       DOUBLE PRECISION DX(*),DY(*)
-#       ..
-#
-#
-# > \par Purpose:
-#  =============
-# >
-# > \verbatim
-# >
-# >    DCOPY copies a vector, x, to a vector, y.
-# >    uses unrolled loops for increments equal to 1.
-# > \endverbatim
-#
-#  Arguments:
-#  ==========
-#
-# > \param[in] N
-# > \verbatim
-# >          N is INTEGER
-# >         number of elements in input vector(s)
-# > \endverbatim
-# >
-# > \param[in] DX
-# > \verbatim
-# >          DX is DOUBLE PRECISION array, dimension ( 1 + ( N - 1 )*abs( INCX ) )
-# > \endverbatim
-# >
-# > \param[in] INCX
-# > \verbatim
-# >          INCX is INTEGER
-# >         storage spacing between elements of DX
-# > \endverbatim
-# >
-# > \param[out] DY
-# > \verbatim
-# >          DY is DOUBLE PRECISION array, dimension ( 1 + ( N - 1 )*abs( INCY ) )
-# > \endverbatim
-# >
-# > \param[in] INCY
-# > \verbatim
-# >          INCY is INTEGER
-# >         storage spacing between elements of DY
-# > \endverbatim
-#
-#  Authors:
-#  ========
-#
-# > \author Univ. of Tennessee
-# > \author Univ. of California Berkeley
-# > \author Univ. of Colorado Denver
-# > \author NAG Ltd.
-#
-# > \date November 2017
-#
-# > \ingroup double_blas_level1
-#
-# > \par Further Details:
-#  =====================
-# >
-# > \verbatim
-# >
-# >     jack dongarra, linpack, 3/11/78.
-# >     modified 12/3/93, array(1) declarations changed to array(*)
-# > \endverbatim
-# >
-#  =====================================================================
 from ..util import slice_
 
 
 def dcopy(N, DX, INCX, DY, INCY):
-    """
-    FIXME
-    """
-    #
-    #  -- Reference BLAS level1 routine (version 3.8.0) --
-    #  -- Reference BLAS is a software package provided by Univ. of Tennessee,    --
-    #  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
-    #     November 2017
-    #
-    #     .. Scalar Arguments ..
-    # INTEGER INCX,INCY,N
-    #     ..
-    #     .. Array Arguments ..
-    # DOUBLE PRECISION DX(*),DY(*)
-    #     ..
-    #
-    #  =====================================================================
+    """Copies a vector, x, to a vector, y
 
+    Parameters
+    ----------
+    N : int
+        number of elements in input vectors
+    DX : numpy.ndarray
+        `DX` is DOUBLE PRECISION array, dimension ( 1 + ( `N` - 1 )*abs( `INCX` ) )
+    INCX : int
+        storage spacing between elements of `DX`
+    DY : numpy.ndarray
+        `DY` is DOUBLE PRECISION array, dimension ( 1 + ( `N` - 1 )*abs( `INCY` ) )
+    INCY : int
+        storage spacing between elements of `DY`
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    Exception
+        If N <= 0
+
+    See Also
+    --------
+    scopy : Single-precision real copy
+    ccopy : Single-precision complex copy
+    zcopy : Double-precision complex copy
+
+    Examples
+    --------
+    >>> x = np.array([1,2,3], dtype=np.double)
+    >>> y = np.array([6,7,8], dtype=np.double)
+    >>> N = len(x)
+    >>> incx = 1
+    >>> incy = 1
+    >>> dcopy(N, x, incx, y, incy)
+    >>> print(y)
+    [1. 2. 3.]
+    """
     if N <= 0:
         return
     DY[slice_(N, INCY)] = DX[slice_(N, INCX)]
