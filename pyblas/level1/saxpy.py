891cc49aa11790586c31f64ad08180c975f68052
Tim Leslie <tim.leslie@gmail.com>
2020-10-12 21:38:32 +1100

    Add saxpy

--- pyblas/level1/saxpy.py
+++ pyblas/level1/saxpy.py
@@ -1,143 +1,50 @@
-# > \brief \b SAXPY
-#
-#  =========== DOCUMENTATION ===========
-#
-# Online html documentation available at
-#            http://www.netlib.org/lapack/explore-html/
-#
-#  Definition:
-#  ===========
-#
-#       def SAXPY(N,SA,SX,INCX,SY,INCY)
-#
-#       .. Scalar Arguments ..
-#       REAL SA
-#       INTEGER INCX,INCY,N
-#       ..
-#       .. Array Arguments ..
-#       REAL SX(*),SY(*)
-#       ..
-#
-#
-# > \par Purpose:
-#  =============
-# >
-# > \verbatim
-# >
-# >    SAXPY constant times a vector plus a vector.
-# >    uses unrolled loops for increments equal to one.
-# > \endverbatim
-#
-#  Arguments:
-#  ==========
-#
-# > \param[in] N
-# > \verbatim
-# >          N is INTEGER
-# >         number of elements in input vector(s)
-# > \endverbatim
-# >
-# > \param[in] SA
-# > \verbatim
-# >          SA is REAL
-# >           On entry, SA specifies the scalar alpha.
-# > \endverbatim
-# >
-# > \param[in] SX
-# > \verbatim
-# >          SX is REAL array, dimension ( 1 + ( N - 1 )*abs( INCX ) )
-# > \endverbatim
-# >
-# > \param[in] INCX
-# > \verbatim
-# >          INCX is INTEGER
-# >         storage spacing between elements of SX
-# > \endverbatim
-# >
-# > \param[in,out] SY
-# > \verbatim
-# >          SY is REAL array, dimension ( 1 + ( N - 1 )*abs( INCY ) )
-# > \endverbatim
-# >
-# > \param[in] INCY
-# > \verbatim
-# >          INCY is INTEGER
-# >         storage spacing between elements of SY
-# > \endverbatim
-#
-#  Authors:
-#  ========
-#
-# > \author Univ. of Tennessee
-# > \author Univ. of California Berkeley
-# > \author Univ. of Colorado Denver
-# > \author NAG Ltd.
-#
-# > \date November 2017
-#
-# > \ingroup single_blas_level1
-#
-# > \par Further Details:
-#  =====================
-# >
-# > \verbatim
-# >
-# >     jack dongarra, linpack, 3/11/78.
-# >     modified 12/3/93, array(1) declarations changed to array(*)
-# > \endverbatim
-# >
-#  =====================================================================
-def SAXPY(N, SA, SX, INCX, SY, INCY):
-    #
-    #  -- Reference BLAS level1 routine (version 3.8.0) --
-    #  -- Reference BLAS is a software package provided by Univ. of Tennessee,    --
-    #  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
-    #     November 2017
-    #
-    #     .. Scalar Arguments ..
-    # REAL SA
-    # INTEGER INCX,INCY,N
-    #     ..
-    #     .. Array Arguments ..
-    # REAL SX(*),SY(*)
-    #     ..
-    #
-    #  =====================================================================
-    #
-    #     .. Local Scalars ..
-    # INTEGER I,IX,IY,M,MP1
-    #     ..
-    #     .. Intrinsic Functions ..
-    # INTRINSIC MOD
-    #     ..
+from ..util import slice_
+
+
+def saxpy(N, SA, SX, INCX, SY, INCY):
+    """Adds a vector x times a constant alpha to a vector y
+
+    Parameters
+    ----------
+    N : int
+        Number of elements in input vector
+    SA : numpy.single
+        Specifies the scalar alpha
+    SX : numpy.ndarray
+        A single precision real array, dimension (1 + (`N` - 1)*abs(`INCX`))
+    INCX : int
+        Storage spacing between elements of `SX`
+    SY : numpy.ndarray
+        A single precision real array, dimension (1 + (`N` - 1)*abs(`INCY`))
+    INCY : int
+        Storage spacing between elements of `SY`
+
+    Returns
+    -------
+    None
+
+    See Also
+    --------
+    daxpy : Double-precision real adding a scaled vector to a vector
+    caxpy : Single-precision complex adding a scaled vector to a vector
+    zaxpy : Double-precision complex adding a scaled vector to a vector
+
+    Notes
+    -----
+    Online PyBLAS documentation: https://nbviewer.jupyter.org/github/timleslie/pyblas/blob/main/docs/saxpy.ipynb
+    Reference BLAS documentation: https://github.com/Reference-LAPACK/lapack/blob/v3.9.0/BLAS/SRC/saxpy.f
+
+    Examples
+    --------
+    >>> x = np.array([1, 2, 3], dtype=np.single)
+    >>> y = np.array([6, 7, 8], dtype=np.single)
+    >>> N = len(x)
+    >>> alpha = 5
+    >>> incx = 1
+    >>> saxpy(N, alpha, x, incx, y, incy)
+    >>> print(y)
+    [11. 17. 23.]
+    """
     if N <= 0:
         return
-    if SA == 0.0:
-        return
-    if INCX == 1 and INCY == 1:
-        # code for both increments equal to 1
-
-        # clean-up loop
-        M = N % 4
-        if M != 0:
-            for I in range(M):
-                SY[I] += SA * SX[I]
-        if N < 4:
-            return
-        for I in range(M, N, 4):
-            SY[I] += SA * SX[I]
-            SY[I + 1] += SA * SX[I + 1]
-            SY[I + 2] += SA * SX[I + 2]
-            SY[I + 3] += SA * SX[I + 3]
-    else:
-        # code for unequal increments or equal increments not equal to 1
-        IX = 1
-        IY = 1
-        if INCX < 0:
-            IX = (-N + 1) * INCX + 1
-        if INCY < 0:
-            IY = (-N + 1) * INCY + 1
-        for I in range(N):
-            SY[IY] += SA * SX[IX]
-            IX += INCX
-            IY += INCY
+    SY[slice_(N, INCY)] += SA * SX[slice_(N, INCX)]
